b46867bd488c2141ab30e9fe90982464762497de
riaandebeer <255699974+infinityabundance@users.noreply.github.com>
2026-01-29 05:46:49 +0000

    Update common.cpp
    17.1

--- libkpulse/src/common.cpp
+++ libkpulse/src/common.cpp
@@ -1,9 +1,15 @@
-#include <QObject>
+#include "kpulse/common.hpp"
+
+#include <QTimeZone>
 
 namespace kpulse {
 
-void placeholder()
+QDateTime nowUtc()
 {
+    // Qt6-friendly way to get current UTC time.
+    return QDateTime::currentDateTimeUtc();
+    // Alternatively:
+    // return QDateTime::currentDateTime().toTimeZone(QTimeZone::utc());
 }
 
 } // namespace kpulse

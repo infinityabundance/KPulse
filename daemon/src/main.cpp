0ab53a15d60dfb99edcad5579259db2ceb9aac48
riaandebeer <255699974+infinityabundance@users.noreply.github.com>
2026-01-29 06:19:27 +0000

    Update main.cpp
    17.1

--- daemon/src/main.cpp
+++ daemon/src/main.cpp
@@ -1,9 +1,48 @@
 #include <QCoreApplication>
+#include <QCommandLineParser>
+#include <QStandardPaths>
+#include <QDir>
 #include <QDebug>
+
+#include "kpulse_daemon.hpp"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication app(argc, argv);
-    qInfo() << "KPulse daemon stub";
-    return 0;
+    app.setApplicationName(QStringLiteral("kpulse-daemon"));
+    app.setOrganizationName(QStringLiteral("KPulse"));
+
+    QCommandLineParser parser;
+    parser.setApplicationDescription(QStringLiteral("KPulse system events daemon"));
+    parser.addHelpOption();
+    parser.addVersionOption();
+
+    QCommandLineOption dbOpt(
+        QStringList() << QStringLiteral("d") << QStringLiteral("database"),
+        QStringLiteral("Path to the KPulse events database (SQLite file)."),
+        QStringLiteral("path")
+    );
+    parser.addOption(dbOpt);
+
+    parser.process(app);
+
+    QString dbPath = parser.value(dbOpt);
+    if (dbPath.isEmpty()) {
+        QString dataDir =
+            QStandardPaths::writableLocation(QStandardPaths::AppDataLocation);
+        if (dataDir.isEmpty()) {
+            dataDir = QDir::homePath() + QStringLiteral("/.local/share/kpulse");
+        }
+        QDir().mkpath(dataDir);
+        dbPath = dataDir + QStringLiteral("/events.sqlite");
+    }
+
+    kpulse::KPulseDaemon daemon(dbPath);
+    if (!daemon.init()) {
+        qCritical() << "KPulse daemon: failed to initialise, exiting";
+        return 1;
+    }
+
+    qInfo() << "KPulse daemon running, DB at" << dbPath;
+    return app.exec();
 }

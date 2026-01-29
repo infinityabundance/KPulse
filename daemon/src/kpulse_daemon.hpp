9e26da47c144103ef2f65fdddcd37af6bf3a141d
riaandebeer <255699974+infinityabundance@users.noreply.github.com>
2026-01-29 05:45:37 +0000

    Update kpulse_daemon.hpp
    17.1

--- daemon/src/kpulse_daemon.hpp
+++ daemon/src/kpulse_daemon.hpp
@@ -4,6 +4,7 @@
 #include <QString>
 #include <QStringList>
 #include <QDateTime>
+
 #include "kpulse/db.hpp"
 #include "kpulse/event.hpp"
 
@@ -17,17 +18,29 @@ class KPulseDaemon : public QObject
     Q_OBJECT
 public:
     explicit KPulseDaemon(const QString &dbPath, QObject *parent = nullptr);
-    ~KPulseDaemon() override = default;
 
+    // Initialise the event store and any other resources.
     bool init();
+
+    // DBus-exposed method used by the generated DaemonAdaptor.
+    // Returns a JSON array (as a compact string) of events that fall within
+    // the given time range and category filter.
+    QString GetEvents(qlonglong fromMs,
+                      qlonglong toMs,
+                      const QStringList &categories);
+
+signals:
+    // Emitted whenever a new event is stored. The DBus adaptor maps this
+    // to the "EventAdded" signal defined in dbus_interface.xml.
+    void EventAdded(const QString &eventJson);
 
 private slots:
     void handleEventDetected(const kpulse::Event &event);
 
 private:
-    QString dbPath_;
-    EventStore store_;
-    JournaldReader journald_;
+    QString         dbPath_;
+    EventStore      store_;
+    JournaldReader  journald_;
     MetricsCollector metrics_;
 };
 

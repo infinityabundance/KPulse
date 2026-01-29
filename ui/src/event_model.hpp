a676fcd81ba0877e284117658a7d1e00bd985f69
riaandebeer <255699974+infinityabundance@users.noreply.github.com>
2026-01-29 02:41:49 +0000

    Add anomaly scoring and visual emphasis

---
+++ ui/src/event_model.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <QAbstractTableModel>
+#include <vector>
+
+#include "kpulse/event.hpp"
+
+class EventModel : public QAbstractTableModel
+{
+    Q_OBJECT
+public:
+    explicit EventModel(QObject *parent = nullptr);
+
+    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+    int columnCount(const QModelIndex &parent = QModelIndex()) const override;
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+    QVariant headerData(int section, Qt::Orientation orientation,
+                        int role = Qt::DisplayRole) const override;
+
+    void setEvents(std::vector<kpulse::Event> events);
+    void appendEvent(const kpulse::Event &event);
+    const kpulse::Event &eventAt(int row) const;
+    bool exportToJson(const QString &filePath) const;
+    bool exportToCsv(const QString &filePath) const;
+
+private:
+    std::vector<kpulse::Event> events_;
+};
